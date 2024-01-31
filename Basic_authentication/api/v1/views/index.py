57a3143515235d60776587318c7ac06f8f737fab
jbizima <j.bizima@alustudent.com>
2024-01-31 12:53:30 +0200

    task1

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -14,13 +14,27 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes=False)
+@app_views.route('/stats/', methods=['GET'], strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
     Return:
-      - the number of each objects
+      - the number of each object
     """
     from models.user import User
-    stats = {}
-    stats['users'] = User.count()
+    stats = {'users': User.count()}
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Raises:
+      - 401 error
+    """
+    abort(401)
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app_views.run(host=host, port=port)
