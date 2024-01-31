d41079aa0ffc9a714923c03717e6540a05a84783
jbizima <j.bizima@alustudent.com>
2024-01-31 13:41:54 +0200

    pycodestyle

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
+
 from flask import jsonify, abort
 from api.v1.views import app_views
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
@@ -11,6 +13,7 @@ def status() -> str:
       - the status of the API
     """
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
@@ -23,6 +26,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized/
@@ -30,6 +34,7 @@ def unauthorized() -> str:
       - abort(401)
     """
     abort(401)
+
 
 @app_views.route('/forbidden', strict_slashes=False)
 def forbidden() -> str:
