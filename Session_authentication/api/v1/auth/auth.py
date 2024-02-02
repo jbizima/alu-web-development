cffc7d5f923f31fdc7c76b8b599c04ada1f617a3
jbizima <j.bizima@alustudent.com>
2024-02-02 16:45:19 +0200

    session tasks

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ Class to manage API authentication
 """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -33,3 +34,9 @@ class Auth:
         """Return None
         """
         return None
+
+    def session_cookie(self, request=None):
+        """Return a cookie value from a request
+        """
+        if request:
+            return request.cookies.get(getenv('SESSION_NAME'))
